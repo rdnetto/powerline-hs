0cc4e4d0d02573fea0a0f24e775baae0254178d9
Reuben D'Netto <rdnetto@gmail.com>
2016-11-07 18:35:34 +1100

    Segments.VCS.branch: Fixed ignore_statuses to use the same format as Powerline

--- src/Segments/VCS.hs
+++ src/Segments/VCS.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Segments.VCS where
 
 import Control.Monad (liftM2)
-import Data.Aeson as Aeson (Value(Bool))
+import Data.Aeson as Aeson (Value(Bool, String))
 import Data.Aeson.Types (emptyArray)
-import Data.List (isInfixOf)
 import qualified Data.Map as Map
 import Data.Maybe (maybeToList)
 
@@ -11,7 +12,22 @@ import Aeson_Unpack
 import Segments.Base
 import Util (readProcess)
 
+
 -- TODO: use a library instead of reinventing the wheel
+
+-- Represents the status of a file in a generic VCS.
+data VcsFileStatus = WorkingDirectoryDirty
+                   | IndexDirty
+                   | UntrackedFile
+                   deriving (Eq, Show)
+
+-- Decodes the blacklist of statuses from the format Powerline uses.
+instance ValueType VcsFileStatus where
+    unpackValue (String "D") = WorkingDirectoryDirty
+    unpackValue (String "I") = IndexDirty
+    unpackValue (String "U") = UntrackedFile
+    unpackValue x = error $ show x ++ " is not a valid VcsFileStatus"
+
 
 -- TODO: add support for other VCSs
 branchSegment :: SegmentHandler
@@ -30,20 +46,26 @@ gitBranchSegment args _ = do
 
     return . maybeToList $ liftM2 Segment (flip HighlightGroup Nothing <$> hlGroup) branch
 
-branchStatusGroup :: [String] -> IO (Maybe String)
+branchStatusGroup :: [VcsFileStatus] -> IO (Maybe String)
 branchStatusGroup blacklist = do
     d <- readProcess "git" ["status", "--porcelain"]
 
     return $ do
         dat <- d
 
-        let fileStatuses = head . words <$> lines dat
-            isNotMasked s = not $ any (`isInfixOf` s) blacklist
+        let fileStatuses = gitToVcsStatus . take 2 <$> lines dat
+            isNotMasked = not . flip elem blacklist
             isDirty = const True
 
         return $ if   any isDirty (filter isNotMasked fileStatuses)
                  then "branch_dirty"
                  else "branch_clean"
+
+-- Converts git status porcelain format to VcsFileStatus
+gitToVcsStatus :: String -> VcsFileStatus
+gitToVcsStatus "??" = UntrackedFile
+gitToVcsStatus [i, ' '] | i /= ' ' = IndexDirty
+gitToVcsStatus _ = WorkingDirectoryDirty
 
 gitBranch :: IO (Maybe String)
 gitBranch = do
@@ -62,6 +84,6 @@ gitStashCount = (=<<) (showNZ . length . lines) <$> readProcess "git" ["stash", 
 statusColors :: SegmentArgs -> Bool
 statusColors = unpackValue . Map.findWithDefault (Bool False) "status_colors"
 
-ignoreStatuses :: SegmentArgs -> [String]
+ignoreStatuses :: SegmentArgs -> [VcsFileStatus]
 ignoreStatuses = unpackValue . Map.findWithDefault emptyArray "ignore_statuses"
 
